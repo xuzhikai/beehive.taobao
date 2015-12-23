8c5a6babb32a2a57f197e2e882aca87e008443b5
zhouguopeng <850583467@qq.com>
2015-12-23 15:44:54 +0800

    添加model

---
+++ model/src/main/java/com/beehive/erp/model/Item.java
@@ -0,0 +1,156 @@
+package com.beehive.erp.model;
+
+import java.math.BigDecimal;
+
+public class Item {
+    private Integer sid;
+
+    private String itemId;
+
+    private String title;
+
+    private BigDecimal price;
+
+    private BigDecimal oriPrice;
+
+    private String goodsNo;
+
+    private Integer auctionStatus;
+
+    private Integer quantity;
+
+    private Integer limitBuy;
+
+    private String auctionDesc;
+
+    private String categoryId;
+
+    private Integer oldQuantity;
+
+    private String createTime;
+
+    private String modifyTime;
+
+    private Integer shopid;
+
+    public Integer getSid() {
+        return sid;
+    }
+
+    public void setSid(Integer sid) {
+        this.sid = sid;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getOriPrice() {
+        return oriPrice;
+    }
+
+    public void setOriPrice(BigDecimal oriPrice) {
+        this.oriPrice = oriPrice;
+    }
+
+    public String getGoodsNo() {
+        return goodsNo;
+    }
+
+    public void setGoodsNo(String goodsNo) {
+        this.goodsNo = goodsNo == null ? null : goodsNo.trim();
+    }
+
+    public Integer getAuctionStatus() {
+        return auctionStatus;
+    }
+
+    public void setAuctionStatus(Integer auctionStatus) {
+        this.auctionStatus = auctionStatus;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getLimitBuy() {
+        return limitBuy;
+    }
+
+    public void setLimitBuy(Integer limitBuy) {
+        this.limitBuy = limitBuy;
+    }
+
+    public String getAuctionDesc() {
+        return auctionDesc;
+    }
+
+    public void setAuctionDesc(String auctionDesc) {
+        this.auctionDesc = auctionDesc == null ? null : auctionDesc.trim();
+    }
+
+
+    public Integer getOldQuantity() {
+        return oldQuantity;
+    }
+
+    public void setOldQuantity(Integer oldQuantity) {
+        this.oldQuantity = oldQuantity;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(String modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public Integer getShopid() {
+        return shopid;
+    }
+
+    public void setShopid(Integer shopid) {
+        this.shopid = shopid;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+}
